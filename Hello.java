622eebf73f130d8dd95951dc32619db52527bc95
jhashahab <99710100+jhashahab@users.noreply.github.com>
2022-02-15 11:23:13 +0530

    01. Hello World : Coding Ninja

--- Hello.java
+++ Hello.java
@@ -1,3 +1,18 @@
+/***************************************************************************************************
+****************************************************************************************************
+Problem : How to Print Anything in java .
+Algorithm : 
+1.Use System.out.println method to print any thing ,
+2.or You can print it with System.out.print .
+
+Time Complexity : O(1)
+Space COmplexity : O(1)
+
+****************************************************************************************************
+***************************************************************************************************/
+
+
+
 class Hello{
 public static void main(String arguments[]){
 System.out.println("Hello Baba");
